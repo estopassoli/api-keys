f332b8e896aee0095c679a8bdd20da04ae0eac36
estopassoli <erick110999@gmail.com>
2022-07-02 17:15:04 -0300

    xd

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express();
 const fs = require('fs');
 const path = require('path')
 const bodyParser = require('body-parser');
+require('dotenv').config()
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html')
-
+const apikey = process.env.APIKEY;
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -21,7 +22,6 @@ app.listen(3000, () => {
 
 app.get('/getkey', (req, res) => {
     const keys = JSON.parse(fs.readFileSync('./keys.json', 'utf8'));
-
     const key = keys[req.query.key];
     if (key) {
         res.send(key);
@@ -30,26 +30,27 @@ app.get('/getkey', (req, res) => {
     }
 });
 app.get('/newkey', (req, res) => {
-    var keys = JSON.parse(fs.readFileSync('./keys.json', 'utf8'));
-
-    //generate a new key with 12 characters
-    var newKey = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (var i = 0; i < 12; i++)
-
-        newKey += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    //check if the key already exists
-    if (keys.hasOwnProperty(newKey)) {
-        res.send("Key already exists");
+    if (req.query.apikey == apikey) {
+        var keys = JSON.parse(fs.readFileSync('./keys.json', 'utf8'));
+        //generate a new key with 12 characters
+        var newkey = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for (var i = 0; i < 22; i++)
+            newkey += possible.charAt(Math.floor(Math.random() * possible.length));
+        //check if the key already exists
+        if (keys.hasOwnProperty(newkey)) {
+            res.send("Key already exists");
+        } else {
+            keys[newkey] = {
+                "value": newkey,
+                "valid": true,
+                "initialDate": new Date().toLocaleString(),
+                "expireDate": new Date(new Date().getTime() + (1000 * 60 * 60 * 24 * 30)).toLocaleString()
+            };
+            fs.writeFileSync('./keys.json', JSON.stringify(keys, null, 2));
+            res.json(keys[newkey]);
+        }
     } else {
-        keys[newKey] = {
-            "key": newKey,
-            "isValid": true,
-            "date": new Date().toLocaleString()
-        };
-        fs.writeFileSync('./keys.json', JSON.stringify(keys, null, 2));
-        res.send(newKey);
+        res.send('Wrong API key');
     }
-
 })
